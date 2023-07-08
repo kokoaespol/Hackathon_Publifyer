3567ed263780426caa7a23c317e853f97ff9431b
Braulio Rivas Abad <brauliorivas2004@gmail.com>
2023-07-08 12:54:36 -0500

    screen 2

---
+++ frontend/src/components/ContentGuide.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="grid grid-cols-2 gap-4">
+        <p>g</p>
+        <p>p</p>
+    </div>
+</template>
--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,12 +1,14 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 import LandingPage from "./components/LandingPage.vue";
+import ContentGuide from "./components/ContentGuide.vue";
 
 export default function router() {
   return createRouter({
     history: createWebHistory(),
     routes: [
       { path: "/", component: LandingPage, },
+      { path: "/guide", component: ContentGuide, }
     ],
   });
 }
