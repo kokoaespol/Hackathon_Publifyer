7995a1bb779f2f2fd0bf1b069e7515985efa4488
agcarpio <agcarpio@espol.edu.ec>
2023-07-08 16:21:12 -0500

    chore: finish the navbar

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -3,6 +3,8 @@ import { createRouter, createWebHistory } from "vue-router";
 import LandingPage from "./components/LandingPage.vue";
 import NewGenie from "./components/NewGenie.vue";
 import Acerca from "./components/Acerca.vue"
+import InicioSesion from './components/InicioSesion.vue'
+import Registro from './components/Registro.vue'
 
 export default function router() {
   return createRouter({
@@ -11,6 +13,8 @@ export default function router() {
       { path: "/", component: LandingPage, },
       { path: "/new", component: NewGenie, },
       { path: "/acerca", component: Acerca,},
+      { path: "/registro", component: Registro,},
+      { path: "/sesion", component: InicioSesion,},
     ],
   });
 }
