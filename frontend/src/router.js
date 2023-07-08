458dd149f8a3a86d62be582c4c875c4f8d417623
wgcotera <wgcotera@espol.edu.ec>
2023-07-08 12:52:57 -0500

    fix(frontEnd): tailwindCss config

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,12 +1,14 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 import LandingPage from "./components/LandingPage.vue";
+import NewGenie from "./components/NewGenie.vue";
 
 export default function router() {
   return createRouter({
     history: createWebHistory(),
     routes: [
       { path: "/", component: LandingPage, },
+      { path: "/new", component: NewGenie, },
     ],
   });
 }
