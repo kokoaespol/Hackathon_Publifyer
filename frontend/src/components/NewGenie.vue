a64b2b3fcbfe86788f80b58863d728b7065d36ab
wgcotera <wgcotera@espol.edu.ec>
2023-07-08 13:25:21 -0500

    chore(frontEnd): add color template

--- frontend/src/components/NewGenie.vue
+++ frontend/src/components/NewGenie.vue
@@ -1,11 +1,26 @@
 <script setup>
 import { inject } from "vue"
-
 const genieService = inject("services.genie");
+
+const name = 'Alina'
 </script>
 
 <template>
-    <div class="bg-red-500">
-        <h1>holiwis</h1>
-    </div>
+    <section class="container w-full h-full p-10 bg-color1F ">
+        <!-- Navbar -->
+
+        <!-- Title -->
+        <div>
+            <h1>{{ name + ", que quieres publicar hoy?" }}</h1>
+        </div>
+        <!-- Breadcrumbs -->
+
+        <!-- Content -->
+        <div class="relative">
+            <div class="">
+                <span class="w-2/4 text-right">Tipo de contenido</span>
+                <input class="border rounded" />
+            </div>
+        </div>
+    </section>
 </template>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,6 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx,vue}",
   ],
   theme: {
+    colors: {
+      'color1F': '#fc27c1',
+      'color2Y': '#fbff99',
+      'color3G': '#8CFFC9',
+      'color4O': '#ff8573',
+      'color5P': '#C280FF',
+    },
     extend: {},
   },
   plugins: [],
