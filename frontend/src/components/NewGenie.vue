4aaec73450524f15bfd15b765598b5e194055886
wgcotera <wgcotera@espol.edu.ec>
2023-07-08 15:47:57 -0500

    chore(frontEnd): effects on inputs

--- frontend/src/components/NewGenie.vue
+++ frontend/src/components/NewGenie.vue
@@ -9,7 +9,7 @@ const name = 'Alina'
 </script>
 
 <template>
-    <section class="relative w-full h-full p-10 bg-color1F bg-opacity-30">
+    <section class="relative w-full h-full p-10 bg-opacity-30">
         <!-- Navbar -->
 
         <!-- Title -->
@@ -26,7 +26,8 @@ const name = 'Alina'
             <CustomInput label="Nombre del producto" placeholder="Ejemplo: Pilsener" />
             <div class="w-full flex flex-row justify-between">
                 <span class="h-10 w-1/2 flex items-center justify-end">Idea</span>
-                <textarea class="h-30 border rounded w-1/2 mx-10 text-white p-5"
+                <textarea
+                    class="h-30 border-0 shadow shadow-color5P focus:shadow-color3G hover:shadow-color1F rounded w-1/2 mx-10 text-white p-5"
                     placeholder="Ingresa que es lo que quisieras hacer, lo que te han solicitado mencionar..."></textarea>
             </div>
             <CustomInput label="Publico" placeholder="Ninos, jovenes, amas de casa, publico general..." />
