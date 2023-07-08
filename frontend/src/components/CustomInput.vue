fdc49629273eb768e82e347c626f213365ce349f
wgcotera <wgcotera@espol.edu.ec>
2023-07-08 15:29:45 -0500

    chore(frontEnd): basic form

---
+++ frontend/src/components/CustomInput.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    props: {
+      label: String,
+      placeholder: String,
+    },
+    setup(props) {
+      console.log(props.title)
+    }
+  }
+</script>
+
+<template>
+    <div class="w-full flex flex-row justify-between">
+        <span class="h-10 w-1/2 flex items-center justify-end">{{ label }}</span>
+        <input class="h-10 border rounded w-1/2 mx-10 text-white p-5" :placeholder="placeholder"/>
+    </div>
+</template>
