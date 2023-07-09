f1905da5f4b07b5fe8054c68f499c41cc8c4163f
Alexander Goussas <goussasalexander@gmail.com>
2023-07-08 21:38:06 -0500

    add cors and genies screen

--- frontend/src/components/Genies.vue
+++ frontend/src/components/Genies.vue
@@ -1,37 +1,49 @@
-<script setup></script>
+<script setup>
+import { inject } from "vue";
+
+import Genie from "./Genie.vue";
+
+const genieService = inject("services.genies");
+
+const genies = await genieService.findAll();
+console.log(genies);
+</script>
 
 <template>
-  <section class="flex">
-    <section class="bg-color3G w-[300px]">
-        <div
-        class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-xl font-bold"
+  <section class="flex w-full">
+    <section
+      id="sidebar"
+      class="bg-white w-1/5 shadow-sm h-screen border border-1 border-gray"
+    >
+      <span
+        class="p-2.5 mt-3 flex items-center rounded-md px-4 cursor-pointer text-xl text-color1F"
       >
-      Genie
-      </div>
-      <div
-        class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600"
-      >
-        <i class="fa-solid fa-star"></i>
+        Genie
+      </span>
+      <div class="p-2.5 mt-3 flex items-center rounded-md px-4 cursor-pointer">
+        <i class="fa-solid fa-star text-color1F"></i>
         <router-link to="/genies">
-          <span class="text-[15px] ml-4 text-gray-200"
-            >Mis Genies</span
-          >
+          <span class="ml-4 text-gray-200">Mis Genies</span>
         </router-link>
       </div>
-      <div
-        class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600"
-      >
-        <i class="fa-solid fa-plus "></i>
+      <div class="p-2.5 mt-3 flex items-center rounded-md px-4 cursor-pointer">
+        <i class="fa-solid fa-plus text-color1F"></i>
         <router-link to="/new">
-          <span class="text-[15px] ml-4 text-gray-200"
-            >Nuevo Genie</span
-          >
+          <span class="ml-4 text-gray-200">Nuevo Genie</span>
         </router-link>
       </div>
     </section>
 
-    <section class="bg-color2Y w-full">
-      <h1>hi</h1>
+    <section class="w-full">
+      <!-- Insert navbar -->
+      <h1 class="text-color1F text-xl">Mis Genies</h1>
+      <genie
+        v-for="genie of genies"
+        :key="genie.id"
+        :subject="genie.subject"
+        :brand="genie.brand"
+        status="Verifying"
+      />
     </section>
   </section>
 </template>
