57e2c4936a2f5f3923de8ed6a6d7aa7edeb2d91b
wgcotera <wgcotera@espol.edu.ec>
2023-07-08 19:18:38 -0500

    chore(frontEnd): sign-up start

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -5,6 +5,7 @@ import NewGenie from "./components/NewGenie.vue";
 import Acerca from "./components/Acerca.vue";
 import ContentGuide from "./components/ContentGuide.vue";
 import ContentVideo from "./components/Video2Genie.vue";
+import Registro from "./components/Registro.vue";
 
 export default function router() {
   return createRouter({
@@ -15,6 +16,8 @@ export default function router() {
       { path: "/acerca", component: Acerca },
       { path: "/guide", component: ContentGuide },
       { path: "/video", component: ContentVideo },
+      { path: "/sign-up", component: Registro },
+      { path: "/:pathMatch(.*)*", redirect: "/"}    
     ],
   });
 }
